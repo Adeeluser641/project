c837fa1abefdb56ede6a5f9ae4dd31e42f7cdb35
adeel <adeeldevacnt641@gmail.com>
2024-06-27 23:01:21 +0500

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-lg font-bold">Home</Link>
-        <div>
-          <Link to="/about" className="text-white mx-2">About</Link>
-          <Link to="/contact" className="text-white mx-2">Contact</Link>
-        </div>
+        <div className="text-white text-lg font-bold">Alpha Investments</div>
+        {/* <div className="space-x-4">
+          <Link to="/" className="text-white">Home</Link>
+          <Link to="/about" className="text-white">About</Link>
+          <Link to="/contact" className="text-white">Contact</Link>
+        </div> */}
       </div>
     </nav>
   );
